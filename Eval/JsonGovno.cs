bebc9e6f3c25fc364f7abcc7372d23a3c850939b
Orehouse <orehouse@ya.ru>
2016-11-19 13:07:20 +0300

    govno fix

--- Eval/JsonGovno.cs
+++ Eval/JsonGovno.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
 namespace EvalTask
@@ -12,6 +9,9 @@ namespace EvalTask
     {
         public static string Replace(string json, string expression)
         {
+            if (string.IsNullOrWhiteSpace(json))
+                return expression;
+
             var vars = JObject.Parse(json);
 
             var replaceDictionary = new Dictionary<string, double>();
