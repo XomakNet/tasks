6e80a6ce4bb109fcf0e76ec0d24d549551ad1b5e
Xomak <mail@xomak.net>
2016-11-19 09:59:32 +0300

    Implemented Json1

--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -8,9 +8,22 @@ namespace JsonConversion
 		static void Main()
 		{
 			string json = Console.In.ReadToEnd();
+		    //string json = Console.In.ReadLine();
 			JObject v2 = JObject.Parse(json);
-			//...
-			var v3 = "{ 'version':'3', 'products': 'TODO' }";
+			dynamic v3 = new JObject();
+		    v3.version = "3";
+
+		    var inputProducts = (JObject)v2["products"];
+            dynamic outputProducts = new JArray();
+
+		    foreach (var x in inputProducts)
+		    {
+                dynamic product = new JObject();
+		        product = x.Value;
+                product.id = x.Key;
+                outputProducts.Add(product);
+            }
+		    v3.products = outputProducts;
 			Console.Write(v3);
 		}
 	}
